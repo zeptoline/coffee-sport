18c91fa9085e6190cebeac85c7adf69477d6c9e7
gign0766 <brewal44@gmail.com>
2016-02-22 11:17:14 +0100

    MaJ

--- src/lectureCSV.py
+++ src/lectureCSV.py
@@ -1,8 +1,16 @@
 import csv
 import os
+import mysql.connector
 from installation import Installation
 from activite import Activite
 from equipement import Equipement
+
+db = mysql.connector.connect(   host="infoweb",     # your host, usually localhost
+                                user="E145617E",    # your username
+                                passwd="E145617E",  # your password
+                                db="E145617E")      # name of the data base
+
+cursor = db.cursor()
 
 type_objet={
     'installations.csv' : Installation,
@@ -17,11 +25,13 @@ def lireCSV(nomFichier):
     with open(fname, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
+
             list.append(type_objet[nomFichier](row[0],row[1],row[2],row[3],row[4],row[5]))
+
 
 def afficherList():
     for elt in list:
         elt.afficher()
 
 lireCSV('activites.csv')
-afficherList()
+db.close()
