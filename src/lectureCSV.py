25fa632e516af4be4ee8e8d26aa06da85a442430
gign0766 <brewal44@gmail.com>
2016-02-22 10:07:47 +0100

    MaJ

--- src/lectureCSV.py
+++ src/lectureCSV.py
@@ -1,18 +1,20 @@
 import csv
+import os
 from installation import Installation
 from activite import Activite
 from equipement import Equipement
 
 type_objet={
-    '/hometu/etudiants/h/e/E146187Z/Cours/semestre4/Prod. logiciel/installations-sportives-pdl-master/data/installations.csv' : Installation,
-    '/hometu/etudiants/h/e/E146187Z/Cours/semestre4/Prod. logiciel/installations-sportives-pdl-master/data/activites.csv' : Activite,
-    '/hometu/etudiants/h/e/E146187Z/Cours/semestre4/Prod. logiciel/installations-sportives-pdl-master/data/equipements.csv' : Equipement
+    'installations.csv' : Installation,
+    'activites.csv' : Activite,
+    'equipements.csv' : Equipement
     }
-nomFichier = '/hometu/etudiants/h/e/E146187Z/Cours/semestre4/Prod. logiciel/installations-sportives-pdl-master/data/activites.csv'
+
 list = []
 
-def lireCSV():
-    with open(nomFichier, 'r') as file:
+def lireCSV(nomFichier):
+    fname = os.path.dirname(os.path.abspath(__file__))+"/../data/"+nomFichier
+    with open(fname, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             list.append(type_objet[nomFichier](row[0],row[1],row[2],row[3],row[4],row[5]))
@@ -21,5 +23,5 @@ def afficherList():
     for elt in list:
         elt.afficher()
 
-lireCSV()
+lireCSV('activites.csv')
 afficherList()
