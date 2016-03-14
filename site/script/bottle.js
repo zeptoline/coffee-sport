4572f126bed6a51c525d49ae62cb36ddd8a416a1
gign0766 <brewal44@gmail.com>
2016-03-14 12:22:02 +0100

    chagnement site

--- site/script/bottle.js
+++ site/script/bottle.js
@@ -1,23 +1,24 @@
 $(function(){
   var min = 0;
   var max = 100;
-  function init_button() {
+  $(".donnee").css({"display":"none"});
+
+  function init_button(bt) {
     min = 0;
     max = 100;
-    $('#dec').prop('disabled', true);
-    $('#inc').prop('disabled', false);
+    $('#dec_'+bt).prop('disabled', true);
+    $('#inc_'+bt).prop('disabled', false);
   }
-  init_button();
-  function incr() {
-    $('#dec').prop('disabled', false);
+  function incr(bt) {
+    $('#dec_'+bt).prop('disabled', false);
     min += 100;
     max += 100;
   }
-  function decr() {
+  function decrbt() {
     min -= 100;
     max -= 100;
     if(min == 0)
-      $('#dec').prop('disabled', true);
+    $('#dec_'+bt).prop('disabled', true);
   }
 
 
@@ -32,6 +33,13 @@ $(function(){
         alert("Erreur : responseText: "+request.responseText);
       },
       success  : function(data) {
+
+        var target= $("#"+fichier);
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1);
+
+
 
         var result = [];
         var table = [];
@@ -67,33 +75,119 @@ $(function(){
           }
         });
 
-        $("#data").html(html);
+        $("#data_"+fichier).html(html);
       }
     });
   }
 
 
-
+  // INSTALLATIONS //
   $("#submit_ins").on("click", function() {
-    init_button();
+    init_button("ins");
+    $(".donnee").css({"display":"none"});
+    $("#installations").css({"display":"block"});
     create_table("installations");
   });
-  $("#submit_act").on("click", function() {
-    init_button();
-    create_table("activites");
+  $("#inc_ins").on("click", function() {
+    incr("ins");
+    create_table("installations");
   });
-  $("#submit_eqpt").on("click", function() {
-    init_button();
-    create_table("equipements");
+  $("#dec_ins").on("click", function() {
+    decr("ins");
+    create_table("installations");
   });
 
-  $("#inc").on("click", function() {
-    incr();
+  // ACTIVITES //
+  $("#submit_act").on("click", function() {
+    init_button("act");
+    $(".donnee").css({"display":"none"});
+    $("#activites").css({"display":"block"});
+    create_table("activites");
+  });
+  $("#inc_act").on("click", function() {
+    incr("act");
+    create_table("activites");
+  });
+  $("#dec_act").on("click", function() {
+    decr("act");
+    create_table("activites");
+  });
+
+  // EQUIPEMENTS //
+  $("#submit_eqpt").on("click", function() {
+    init_button("eqpt");
+    $(".donnee").css({"display":"none"});
+    $("#equipements").css({"display":"block"});
     create_table("equipements");
   });
-  $("#dec").on("click", function() {
-    decr();
+  $("#inc_eqpt").on("click", function() {
+    incr("eqpt");
+    create_table("equipements");
+  });
+  $("#dec_eqpt").on("click", function() {
+    decr("eqpt");
     create_table("equipements");
   });
 
 });
+
+
+
+
+
+/*
+(function() // Code in a function to create an isolate scope
+{
+var speed = 500;
+var moving_frequency = 15; // Affects performance !
+var links = document.getElementsByTagName('a');
+var href;
+for(var i=0; i<links.length; i++)
+{
+    href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
+    if(href !== null && href.length > 1 && href.substr(0, 1) == '#')
+    {
+        links[i].onclick = function()
+        {
+            var element;
+            var href = this.attributes.href.nodeValue.toString();
+            if(element = document.getElementById(href.substr(1)))
+            {
+                var hop_count = speed/moving_frequency
+                var getScrollTopDocumentAtBegin = getScrollTopDocument();
+                var gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count;
+
+                for(var i = 1; i <= hop_count; i++)
+                {
+                    (function()
+                    {
+                        var hop_top_position = gap*i;
+                        setTimeout(function(){  window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin); }, moving_frequency*i);
+                    })();
+                }
+            }
+
+            return false;
+        };
+    }
+}
+
+var getScrollTopElement =  function (e)
+{
+    var top = 0;
+
+    while (e.offsetParent != undefined && e.offsetParent != null)
+    {
+        top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+        e = e.offsetParent;
+    }
+
+    return top;
+};
+
+var getScrollTopDocument = function()
+{
+    return document.documentElement.scrollTop + document.body.scrollTop;
+};
+})();
+*/
